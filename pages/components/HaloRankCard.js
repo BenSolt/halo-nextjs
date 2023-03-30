78ea39a470da6bd04cd175e6fd8133a2dd518798
benjamin-solt <bensolt@gmail.com>
2023-03-30 15:25:04 -0400

    fixing tiers

--- pages/components/HaloRankCard.js
+++ pages/components/HaloRankCard.js
@@ -14,7 +14,7 @@ export default function HaloRank({ h }) {
             <div className='TierHolder'>
                 {/* <img  className='Banner'src={h.bannerImageUrl} /> */}
 
-                {h.tiers.map(h => {
+                {h?.tiers?.map(h => {
                     return (
                         <div key={h.id}>
                             <img className="Tier" src={h.iconImageUrl} alt="rank images" />
