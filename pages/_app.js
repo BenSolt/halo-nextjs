70a0cddfdd5c9b88437c52c646394420b586b3e1
benjamin-solt <bensolt@gmail.com>
2023-04-04 14:04:35 -0400

    editing styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/myStyles.css'
-import '@/styles/Player.css'
+import '@/styles/PlayerCard.css'
 import '@/styles/rank.css'
 
 export default function App({ Component, pageProps }) {
--- pages/components/HaloPlayerCard.js
+++ pages/components/HaloPlayerCard.js
@@ -45,63 +45,54 @@ export default function HaloPlayerCard({ p }) {
 
             <div className='Gamertag'>
                 {/* GAMER TAG*/}
-                <h2> {p?.Id}</h2>
+                <h2 className="rankText"> {p?.Id}</h2>
                 <h4 className="rankText"> Rank: {p?.Result?.SpartanRank}</h4>
                 {/* <h1>{rankcurrent}</h1>  */}
             </div>
 
             <div className="info">
                 <div className="infoText">
-                    {HighestRankAttained === null ? (<h4>Highest Rank: 0</h4>) : (<h4>Highest Rank: {HighestRankAttained}</h4>) }
-                    {HRankAttainedDesign === null ? (<h4>Highest Desg: 0</h4>) : (<h4>Highest Rank: {HRankAttainedDesign}</h4>) }
+                    {HighestRankAttained === null ? (<h4>Highest Rank: 0</h4>) : (<h4>Highest Rank: {HighestRankAttained}</h4>)}
+                    {HRankAttainedDesign === null ? (<h4>Highest Desg: 0</h4>) : (<h4>Highest Rank: {HRankAttainedDesign}</h4>)}
                 </div>
 
-                {HRankAttainedDesign === 6 ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array00-783f32318c8c49eda0365c5daa50f5b6.png"></img>
-                ) : HRankAttainedDesign === 7 ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array01-3755e5127c9c46368e648d58da44390e.png"></img>
-                ) : HRankAttainedDesign === null ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/unranked_00-61fca949c33f433ba7e7507d97ff130f.png"></img>
-                ) : HRankAttainedDesign === 4 ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_platinum_array01-c8df3dc366ea49209762f9b08189ffa6.png"></img>
-                ) : HRankAttainedDesign === 2 ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_silver_array01-b4739333586340bda2eed6099e630f92.png"></img>
-                ) : HRankAttainedDesign === 5 ? (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_diamond_array01-9721d95b267942dcb1edcce6dfc25631.png"></img>
-                ) : (
-                    <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_platinum_array01-c8df3dc366ea49209762f9b08189ffa6.png"></img>
-                )
-                }
-            </div>
-
-            <div className="Stats">
-                <div className='textHolder'>
-                    <h6 className='textgold'>GAMES WON: {GamesWon}</h6>
-                </div>
-                <div className='textHolder'>
-                    <h6 className='textgold'>GAMES LOST: {GamesLost}</h6>
-                </div>
-                <div className='textHolder'>
-                    <h6 className='textgold'>GAMES TIED: {GamesTied}</h6>
-                </div>
-            </div>
-            <div className="Stats">
-                <div className='textHolder'>
-                    <h6 className='textgold'>TOTAL KILLS: {TotalKillsPlayer}</h6>
-                </div>
-                <div className='textHolder'>
-                    <h6 className='textgold'>DEATHS: {Deaths}</h6>
+                <div>
+                    {HRankAttainedDesign === 6 ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array00-783f32318c8c49eda0365c5daa50f5b6.png"></img>
+                    ) : HRankAttainedDesign === 7 ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array01-3755e5127c9c46368e648d58da44390e.png"></img>
+                    ) : HRankAttainedDesign === null ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/unranked_00-61fca949c33f433ba7e7507d97ff130f.png"></img>
+                    ) : HRankAttainedDesign === 4 ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_platinum_array01-c8df3dc366ea49209762f9b08189ffa6.png"></img>
+                    ) : HRankAttainedDesign === 2 ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_silver_array01-b4739333586340bda2eed6099e630f92.png"></img>
+                    ) : HRankAttainedDesign === 5 ? (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_diamond_array01-9721d95b267942dcb1edcce6dfc25631.png"></img>
+                    ) : (
+                        <img className="tierCard" src="https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_platinum_array01-c8df3dc366ea49209762f9b08189ffa6.png"></img>
+                    )
+                    }
                 </div>
             </div>
 
             <div className="Stats">
+                <h6 className='textgold'>Games Won: {GamesWon}</h6>
+                <h6 className='textgold'>Games Lost: {GamesLost}</h6>
+            </div>
 
-                <div className='textHolder'>
-                    <h6 className='textgold'>ASSASINATIONS: {Assasinations}</h6>
-                </div>
-                <div className='textHolder'>
-                    <h6 className='textgold'>ASSISTS: {Assists}</h6>
-                </div>
+            <div className="Stats2">
+                <h6 className='textgold'>Games Tied: {GamesTied}</h6>
+            </div>
+
+            <div className="Stats3">
+                <h6 className='textgold'>Total Kills: {TotalKillsPlayer}</h6>
+                <h6 className='textgold'>Deaths: {Deaths}</h6>
+            </div>
+
+            <div className="Stats">
+                <h6 className='textgold'>Assasinations: {Assasinations}</h6>
+                <h6 className='textgold'>Assists: {Assists}</h6>
             </div>
 
             {/* <h6>Total kills Vehic:{TotalKillsV}</h6> */}
