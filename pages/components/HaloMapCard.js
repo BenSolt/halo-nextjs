006badaa8e089ade70825dea8bb60f4a1e99f6a4
benjamin-solt <bensolt@gmail.com>
2023-04-05 11:17:43 -0400

    added conditioning to map and medal cards

--- pages/components/HaloMapCard.js
+++ pages/components/HaloMapCard.js
@@ -5,9 +5,9 @@ export default function HaloMapCard({m}) {
  
     return (
       <div className="card">
-          <h3>{m.name}</h3>
-          <img className="MapImage" src={m.imageUrl} alt="map images"/>
-          <h6>{m.description}</h6>
+          <h3>{m?.name}</h3>
+          <img className="MapImage" src={m?.imageUrl} alt="map images"/>
+          <h6>{m?.description}</h6>
           {/* {m.supportedGameModes} */}
            {/* {m.filter(str => (str.supportedGameModes === "Warzone"))} */}
       </div>
--- pages/components/HaloMedalCard.js
+++ pages/components/HaloMedalCard.js
@@ -6,15 +6,15 @@ export default function HaloMedalCard({ m }) {
     return (
         <div className="card">
 
-            <h3>{m.name}</h3>
-            <h6>{m.description}</h6>
+            <h3>{m?.name}</h3>
+            <h6>{m?.description}</h6>
 
             <div className='spriteholder'>
-                <img className="sprite" src={m.spriteLocation.spriteSheetUri}
+                <img className="sprite" src={m?.spriteLocation.spriteSheetUri}
                     alt='halo medal images'
                     style={{
                         objectFit: "none",
-                        objectPosition: `-${m.spriteLocation.left}px -${m.spriteLocation.top}px`
+                        objectPosition: `-${m?.spriteLocation.left}px -${m?.spriteLocation.top}px`
                     }}
                 />
             </div>
