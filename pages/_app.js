726ff635830d14f231c0971a99a0921adf5854d6
benjamin-solt <bensolt@gmail.com>
2023-04-05 11:19:32 -0400

    added back in playerCard css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/myStyles.css'
-// import '@/styles/playerCard.css'
+import '@/styles/playerCard.css'
 import '@/styles/rank.css'
 import '@/styles/mapCard.css'
 import '@/styles/medals.css'
